fec088db4e5b9ac07ba58d67b5de637d48271f7a
Bàng Nga <bangnga82@gmail.com>
2026-03-11 21:55:27 +0700

    Initial commit

---
+++ src/main/java/com/example/ngerShop_be/NgerShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngerShop_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgerShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgerShopBeApplication.class, args);
+	}
+
+}
