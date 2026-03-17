17a79472d88e6ad85282b7b39f5c5e5063bd751d
Bàng Nga <bangnga82@gmail.com>
2026-03-17 23:35:01 +0700

    update users

--- src/main/java/com/example/ngerShop_be/modules/user/repository/UserRepository.java
+++ src/main/java/com/example/ngerShop_be/modules/user/repository/UserRepository.java
@@ -1,17 +1,24 @@
 package com.example.ngerShop_be.modules.user.repository;
 
+
 import com.example.ngerShop_be.common.constants.UserStatus;
 import com.example.ngerShop_be.modules.user.entity.User;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("""
+        select u from User u
+        left join fetch u.roles
+        where u.email = :email
+        """)
+    Optional<User> findByEmailWithRoles(@Param("email") String email);
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
 
@@ -30,5 +37,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
             @Param("query") String query,
             Pageable pageable
     );
-
 }
