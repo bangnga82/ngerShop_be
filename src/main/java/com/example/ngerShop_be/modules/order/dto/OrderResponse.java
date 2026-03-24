5e736258cb4310f72a3e5d31c850ffadd2fdec8d
Bàng Nga <bangnga82@gmail.com>
2026-03-24 23:13:25 +0700

    update order tiep_24032035

--- src/main/java/com/example/ngerShop_be/modules/order/dto/OrderResponse.java
+++ src/main/java/com/example/ngerShop_be/modules/order/dto/OrderResponse.java
@@ -2,18 +2,23 @@ package com.example.ngerShop_be.modules.order.dto;
 
 import com.example.ngerShop_be.common.constants.OrderStatus;
 import com.example.ngerShop_be.common.constants.PaymentMethod;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record OrderResponse(
-        UUID id,
-        String reference,
-        OrderStatus status,
-        PaymentMethod paymentMethod,
-        Double totalAmount,
-        List<OrderItemResponse> items,
-        Object paymentDetails,
-        LocalDateTime createdDate
-) {}
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponse { // Chuyển thành CLASS
+    UUID id;
+    String reference;
+    OrderStatus status;
+    PaymentMethod paymentMethod;
+    Double totalAmount;
+    List<OrderItemResponse> items;
+    Object paymentDetails;
+    LocalDateTime createdDate;
+}
